67192b60133bd4da07cef85cc43fe9dd388dbcaa
Liwia <liwia@air-liwia.home>
2020-01-22 18:31:53 +0100

    add files

---
+++ build/precache-manifest.d47cf83bec083bbf64dab7a37cf11373.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d753a416966f881578231f70cca11b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4bc5d59114a06a1f0ce4",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "b08c94c83f7d14793b91",
+    "url": "/static/css/main.9fb4f8ae.chunk.css"
+  },
+  {
+    "revision": "4bc5d59114a06a1f0ce4",
+    "url": "/static/js/2.49c6e873.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.49c6e873.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b08c94c83f7d14793b91",
+    "url": "/static/js/main.9fb7831d.chunk.js"
+  },
+  {
+    "revision": "66449bfd90eee63dac39",
+    "url": "/static/js/runtime-main.e285c975.js"
+  },
+  {
+    "revision": "2de9de4d5471eb50dd8cf6c040d6e476",
+    "url": "/static/media/favicon.2de9de4d.svg"
+  },
+  {
+    "revision": "0a64cf6b8e82340db88727b708da0ce3",
+    "url": "/static/media/horse1.0a64cf6b.jpg"
+  },
+  {
+    "revision": "5e4434b5a64f6de717d819f068b42234",
+    "url": "/static/media/horse2.5e4434b5.jpg"
+  },
+  {
+    "revision": "25560b0bb6aed0ded2fb6ab336b7f785",
+    "url": "/static/media/horse3.25560b0b.jpg"
+  }
+]);
